1069d9f19b10524c1fb51d2f06f39d489bb0d42b
WBuddhi <w.deseram@gmail.com>
2019-02-20 21:36:24 +0100

    inserted question answer python module into index view. Working on file upload next

--- HiAriana/QusAns/views.py
+++ HiAriana/QusAns/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
+from . import QusAnswer
+import json
+
 def index(request):
-    return HttpResponse("Ready to start app")
+    file_path = './QusAns/Questions.json'
+    if file_path != None:
+        Qus_Ans = get_qusans(file_path)
+        Statement = Qus_Ans.Statement
+
+        return HttpResponse(Statement)
+    else: return HttpResponse("File not loaded")
+
+def get_qusans(file_path):
+    with open(file_path) as f:
+        Qus_data = json.load(f)
+    Qus_Ans = QusAnswer.QusAns(Qus_data)
+    return Qus_Ans
+
+    
+    
