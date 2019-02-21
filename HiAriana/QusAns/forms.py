35c8e7f8f1ff069156de21a20fdc30358faaa2d1
WBuddhi <w.deseram@gmail.com>
2019-02-21 17:00:00 +0100

    Added new page for questions and answers

--- HiAriana/QusAns/forms.py
+++ HiAriana/QusAns/forms.py
@@ -1,10 +1,4 @@
 from django import forms
-#from QusAns.models import QusJsonFile
-
-#class DocumentForm(forms.ModelForm):
-#    class Meta:
-#        model = QusJsonFile
-#        fields = {'document',}
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
--- HiAriana/QusAns/views.py
+++ HiAriana/QusAns/views.py
@@ -1,19 +1,24 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, reverse
 from .forms import UploadFileForm
 from django.conf import settings
 from django.core.files.storage import default_storage
+from urllib.parse import urlencode
 
 import os
 from . import QusAnswer
 import json
 
+
+def get_qusans(file_path):
+    with open(file_path) as f:
+        Qus_data = json.load(f)
+    Qus_Ans = QusAnswer.QusAns(Qus_data)
+    return Qus_Ans
+
+
 def index(request):
     if request.method == 'POST':
-            # #file_path = './QusAns/Questions.json'
-            # if file_path != None:
-            #     Qus_Ans = get_qusans(file_path)
-            #     Statement = Qus_Ans.Statement
 
         #   Uploading file
         save_path = os.path.join(str(settings.MEDIA_ROOT), 'Qusfiles', 
@@ -24,16 +29,19 @@ def index(request):
         Qus_Ans = get_qusans(path)
         Statement = Qus_Ans.Statement
         
-        return HttpResponse(Statement)
+        base_url = reverse('QusAns:qus_ans')
+        print(base_url)
+        query_string = urlencode({'path':path})
+        url = '{}?{}'.format(base_url, query_string)
+        return redirect(url)
 
     else: form = UploadFileForm()
     return render(request, 'QusAns/index.html', {'form':form})
 
-def get_qusans(file_path):
-    with open(file_path) as f:
-        Qus_data = json.load(f)
-    Qus_Ans = QusAnswer.QusAns(Qus_data)
-    return Qus_Ans
-
-    
+def qus(request):
+    path = request.GET.get('path')
+    Qus_Ans = get_qusans(path)
+    Statement = Qus_Ans.Statement
+    Answers = Qus_Ans.Answers
+    return HttpResponse(Statement)
     
